873baea5d792612d3e5f2d729e64c53191ee21b3
captaincookie886 <45984741+Tehcnalties@users.noreply.github.com>
2025-11-19 21:12:50 -0600

    create the grid

---
+++ src/snake/GridManager.java
@@ -0,0 +1,72 @@
+package snake;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
+
+public class GridManager {
+    private final Color LIGHT_ORANGE = Color.decode("#ffb38a");
+    private final Color DARK_ORANGE = Color.decode("#ff9248");
+    private Color color;
+
+    private CanvasWindow canvas;
+    private GraphicsGroup grid;
+    private List<GridSquare> gridSquares;
+
+    public GridManager(CanvasWindow canvas) {
+        gridSquares = new ArrayList<>();
+        this.canvas = canvas;
+    }
+
+    /**
+     * Creates the grid that the apples and snake will be on
+     */
+    public void createGrid() {
+        grid = new GraphicsGroup();
+
+        int gridWidth = 19;
+        int gridHeight = 19;
+        int totalGridSquares = gridWidth * gridHeight;
+
+        double x = 0;
+        double y = 0;
+
+        int gridX = 1;
+        int gridY = 1;
+
+        for(int i = 0; i < totalGridSquares; i++) {
+            if(isEven(i)) {
+                color = DARK_ORANGE;
+            } else {
+                color = LIGHT_ORANGE;
+            }
+
+            GridSquare gridSquare = new GridSquare(x, y, gridX, gridY, color, canvas);
+
+            gridSquares.add(gridSquare);
+            grid.add(gridSquare.getGraphics());
+
+            x += gridSquare.getGraphics().getWidth();
+            gridX += 1;
+
+            if(x >= canvas.getWidth()) {
+                x = 0;
+                gridX = 1;
+                y += gridSquare.getGraphics().getHeight();
+                gridY += 1;
+            }
+        }
+
+        canvas.add(grid);
+    }
+
+    private boolean isEven(int x) {
+        if(x % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/snake/SnakeGame.java
@@ -0,0 +1,29 @@
+package snake;
+
+import java.awt.Color;
+import edu.macalester.graphics.CanvasWindow;
+
+public class SnakeGame {
+    private static final int CANVAS_WIDTH = 570;
+    private static final int CANVAS_HEIGHT = 570;
+
+    /*
+     start with a 19 x 19 grid for the snake
+     since canvas size is 570x570 each grid square is 30px by 30px
+    */
+
+    private final CanvasWindow canvas;
+    private final GridManager gridManager;
+
+    public SnakeGame() {
+        canvas = new CanvasWindow("SNAKE", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.setBackground(Color.BLUE);
+
+        gridManager = new GridManager(canvas);
+        gridManager.createGrid();
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame();
+    }
+}
